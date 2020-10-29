036bd628f5a2e9788cd34ac324e3099eb56f8972
Andrew Franklin <andfranklin3@gmail.com>
2020-10-29 14:19:10 -0500

    Added tests for ColFaceSlice.

--- tests/test_face_slices.py
+++ tests/test_face_slices.py
@@ -68,10 +68,10 @@ class TestRowFaceSlices(FaceSlicesTests):
     @mark.dependency(depends=["construction"])
     def test_repr(self):
         stickers = self.stickers()
-        face = RowFaceSlice(stickers)
+        face_slice = RowFaceSlice(stickers)
         gold = "\x1b[7m\x1b[1m\x1b[32m ↑ \x1b[0m\x1b[7m\x1b[1m\x1b[33m → "
         gold += "\x1b[0m\x1b[7m\x1b[1m\x1b[35m ↓ \x1b[0m"
-        assert repr(face) == gold, f"{repr(face)}: {repr(repr(face))}"
+        assert repr(face_slice) == gold, f"{repr(face_slice)}: {repr(repr(face_slice))}"
 
     def rotate_cw_test(self):
         stickers = self.stickers()
@@ -102,6 +102,65 @@ class TestRowFaceSlices(FaceSlicesTests):
         orig_slice = RowFaceSlice(stickers)
         face_slice = RowFaceSlice(deepcopy(stickers)).rotate_ht()
         assert isinstance(face_slice, RowFaceSlice)
+
+        err_str = f"\n{repr(orig_slice)})\nto\n{repr(face_slice)}"
+        sol_stickers = face_slice.stickers
+        assert sol_stickers[0] == Sticker(FaceEnum.LEFT, OrientEnum.UP), err_str
+        assert sol_stickers[1] == Sticker(FaceEnum.BACK, OrientEnum.LEFT), err_str
+        assert sol_stickers[2] == Sticker(FaceEnum.FRONT, OrientEnum.DOWN), err_str
+
+
+class TestColFaceSlices(FaceSlicesTests):
+    """Collection of all tests run on instances of the ColFaceSlices Class."""
+
+    class_ = ColFaceSlice
+    objs = col_face_slices
+    objs_minus_c2 = col_face_slices_minus_c2
+    objs_minus_c4 = col_face_slices
+
+    @mark.dependency(depends=["construction"])
+    def test_str(self):
+        stickers = self.stickers()
+        gold = f"ColFaceSlice(N=3)"
+        assert str(ColFaceSlice(stickers)) == gold
+
+    @mark.dependency(depends=["construction"])
+    def test_repr(self):
+        stickers = self.stickers()
+        face_slice = ColFaceSlice(stickers)
+        gold = "\x1b[7m\x1b[1m\x1b[32m ↑ "
+        gold += "\x1b[0m\n\x1b[7m\x1b[1m\x1b[33m → \x1b[0m\n\x1b[7m\x1b[1m\x1b[35m ↓ \x1b[0m"
+        assert repr(face_slice) == gold, f"{repr(face_slice)}: {repr(repr(face_slice))}"
+
+    def rotate_cw_test(self):
+        stickers = self.stickers()
+        orig_slice = ColFaceSlice(stickers)
+        face_slice = ColFaceSlice(deepcopy(stickers)).rotate_cw()
+        assert isinstance(face_slice, RowFaceSlice)
+
+        err_str = f"\n{repr(orig_slice)})\nto\n{repr(face_slice)}"
+        sol_stickers = face_slice.stickers
+        assert sol_stickers[0] == Sticker(FaceEnum.LEFT, OrientEnum.LEFT), err_str
+        assert sol_stickers[1] == Sticker(FaceEnum.BACK, OrientEnum.DOWN), err_str
+        assert sol_stickers[2] == Sticker(FaceEnum.FRONT, OrientEnum.RIGHT), err_str
+
+    def rotate_ccw_test(self):
+        stickers = self.stickers()
+        orig_slice = ColFaceSlice(stickers)
+        face_slice = ColFaceSlice(deepcopy(stickers)).rotate_ccw()
+        assert isinstance(face_slice, RowFaceSlice)
+
+        err_str = f"\n{repr(orig_slice)})\nto\n{repr(face_slice)}"
+        sol_stickers = face_slice.stickers
+        assert sol_stickers[0] == Sticker(FaceEnum.FRONT, OrientEnum.LEFT), err_str
+        assert sol_stickers[1] == Sticker(FaceEnum.BACK, OrientEnum.UP), err_str
+        assert sol_stickers[2] == Sticker(FaceEnum.LEFT, OrientEnum.RIGHT), err_str
+
+    def rotate_ht_test(self):
+        stickers = self.stickers()
+        orig_slice = ColFaceSlice(stickers)
+        face_slice = ColFaceSlice(deepcopy(stickers)).rotate_ht()
+        assert isinstance(face_slice, ColFaceSlice)
 
         err_str = f"\n{repr(orig_slice)})\nto\n{repr(face_slice)}"
         sol_stickers = face_slice.stickers
