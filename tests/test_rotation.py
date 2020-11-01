89205eb5e5c89a2a3a334df321fe0dcba7c638f5
Andrew Franklin <andfranklin3@gmail.com>
2020-11-01 17:52:40 -0600

    Added some more tests of the Rotation class.

--- tests/test_rotation.py
+++ tests/test_rotation.py
@@ -2,6 +2,7 @@ from ErnosCube.rotation import AxisEnum, MagEnum, Rotation
 from strategies import axis_enums, mag_enums, layers, rotations
 from hypothesis import given
 from pytest import mark, raises
+from copy import deepcopy
 
 
 class TestRotation:
@@ -17,3 +18,32 @@ class TestRotation:
     def test_construction_failure(self, axis, mag):
         with raises(AssertionError):
             Rotation(axis, mag, -2)
+
+    @mark.dependency(name="equality", depends=["construction_failure"])
+    def test_equality(self):
+        a = Rotation(AxisEnum.X, MagEnum.CW, 1)
+        assert a == a
+
+        b = Rotation(AxisEnum.X, MagEnum.CW, 2)
+        assert not (a == b)
+
+        c = Rotation(AxisEnum.X, MagEnum.CCW, 1)
+        assert not (a == c)
+
+        d = Rotation(AxisEnum.Y, MagEnum.CW, 1)
+        assert not (a == d)
+
+    @mark.dependency(name="inequality", depends=["equality"])
+    @given(rotations, rotations)
+    def test_inequality(self, a, b):
+        if a == b:
+            assert not (a != b)
+        else:
+            assert a != b
+
+    @mark.dependency(name="deepcopy", depends=["equality"])
+    @given(rotations)
+    def test_deepcopy(self, rotation):
+        rotation_copy = deepcopy(rotation)
+        assert rotation is not rotation_copy
+        assert rotation == rotation_copy
